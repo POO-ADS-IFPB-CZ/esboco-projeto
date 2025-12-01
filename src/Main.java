5e16f406fc22679c50a1263c978475cd57de0a7c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-12-01 08:25:13 -0300

    Testando os métodos

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,22 @@ public class Main {
 
     public static void main(String[] args) {
         JogadorDao jogadorDao = new JogadorDao();
-        //Após ler os dados do jogador
-        if(jogadorDao.salvar(
-                new Jogador("111.111.111-01", "Danilo",
-                        LocalDate.of(1983, 3,2),
-                        "Flamengo", "Zagueiro",
-                        10000000)
-        )){
+
+        //Após ler os dados do jogador (formulário)
+        Jogador jogador = new Jogador("111.111.111-01",
+                "Danilo",
+                LocalDate.of(1983, 3,2),
+                "Flamengo", "Zagueiro",
+                10000000);
+
+        if(jogadorDao.salvar(jogador)){
             System.out.println("Salvo com sucesso!");
         }
+        System.out.println(jogadorDao.getJogadores());
+        jogador.setNome("Danilo bi campeão");
+        jogadorDao.atualizar(jogador);
+        System.out.println(jogadorDao.getJogadores());
+        jogadorDao.deletar(jogador);
         System.out.println(jogadorDao.getJogadores());
     }
 
