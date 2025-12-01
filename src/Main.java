4c47b6cce5883821f11b0028ac4fa948a823441a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-12-01 09:15:14 -0300

    Testando os métodos de salvar e listar

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,25 @@ import dao.JogadorDao;
 import model.Jogador;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
-        JogadorDao jogadorDao = new JogadorDao();
-
-        //Após ler os dados do jogador (formulário)
-        Jogador jogador = new Jogador("111.111.111-01",
-                "Danilo",
-                LocalDate.of(1983, 3,2),
-                "Flamengo", "Zagueiro",
-                10000000);
-
-        if(jogadorDao.salvar(jogador)){
-            System.out.println("Salvo com sucesso!");
+        try {
+            JogadorDao dao = new JogadorDao();
+            System.out.println(dao.getJogadores());
+//            dao.salvar(new Jogador("111.111.111-01",
+//                "Danilo",
+//                    LocalDate.of(1990,2,3),
+//                    "Flamengo", "Zagueiro",
+//                    10000000));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println(jogadorDao.getJogadores());
-        jogador.setNome("Danilo bi campeão");
-        jogadorDao.atualizar(jogador);
-        System.out.println(jogadorDao.getJogadores());
-        jogadorDao.deletar(jogador);
-        System.out.println(jogadorDao.getJogadores());
     }
 
 }
